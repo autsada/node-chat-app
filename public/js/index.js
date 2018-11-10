a035baeaf17362abc18789cfd1e7227c58a507b3
Autsada <autsada.t@gmail.com>
2018-11-10 20:17:07 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-      from: 'Auddy',
-      text: 'Hello'
-  })
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -19,14 +19,17 @@ app.use(express.static(publicPath))
 io.on('connection', socket => {
     console.log('New user connected')
 
-    socket.emit('newMessage', {
-        from: 'Julia',
-        text: 'Hello, how are you doing',
-        createdAt: 1000
-    })
+    // socket.emit -> emit event to single connection
 
     socket.on('createMessage', message => {
         console.log('createMessage', message)
+
+        // io.emit -> emit event to all connections
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     socket.on('disconnect', () => {
