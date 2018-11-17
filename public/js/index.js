526dab53bb079eb38d729332f87728aa5c1aeb47
Autsada <autsada.t@gmail.com>
2018-11-17 18:17:47 +0700

    Add scrollToBottom method

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 let socket = io();
 
+function scrollToBottom () {
+  // Selectors
+  const messages = jQuery('#messages')
+  const newMessage = messages.children('li:last-child')
+
+  // Heights
+  const clientHeight = messages.prop('clientHeight')
+  const scrollTop = messages.prop('scrollTop')
+  const scrollHeight = messages.prop('scrollHeight')
+  const newMessageHeight = newMessage.innerHeight()
+  const lastMessageHeight = newMessage.prev().innerHeight()
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -18,6 +35,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom()
 
   // const formatedTime = moment(message.createdAt).format('h:mm a')
 
@@ -37,6 +55,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom()
 
   // let li = jQuery('<li></li>');
   // let a = jQuery('<a target="_blank" >My current location</a>');
