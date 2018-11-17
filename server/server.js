69aaaaff89577ce5cf4c54225967c389561ca0d0
Autsada <autsada.t@gmail.com>
2018-11-17 13:27:17 +0700

    Add css

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on('connection', socket => {
 
         // io.emit -> emit event to all connections
         io.emit('newMessage', generateMessage(message.from, message.text))
-        callback('This is from the server')
+        callback()
     })
 
     socket.on('createLocationMessage', coords => {
