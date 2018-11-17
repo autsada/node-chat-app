05e31c25d06cf9182d388aad93a535cbfea1daf1
Autsada <autsada.t@gmail.com>
2018-11-17 11:38:38 +0700

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -4,19 +4,15 @@ const path = require('path') // path is a node build-in module
 const express = require('express')
 const socketIO = require('socket.io')
 
-const {generateMessage} = require('./utils/message')
+const {generateMessage, generateLocationMessage} = require('./utils/message')
 
 const publicPath = path.join(__dirname, '../public')
-//const publicPath = path.join(__dirname, '../public/index.html')
 const port = process.env.PORT
 let app = express()
 let server = http.createServer(app)
 let io = socketIO(server)
 
 app.use(express.static(publicPath))
-// app.get('/', (req, res) => {
-//     res.sendFile(publicPath)
-// })
 
 io.on('connection', socket => {
     console.log('New user connected')
@@ -24,6 +20,7 @@ io.on('connection', socket => {
     // Welcome message -> socket.emit -> emit event to single connection
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'))
 
+    // socket.broadcast.emit -> send message to everyone except creator
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
     socket.on('createMessage', (message, callback) => {
@@ -32,13 +29,11 @@ io.on('connection', socket => {
         // io.emit -> emit event to all connections
         io.emit('newMessage', generateMessage(message.from, message.text))
         callback('This is from the server')
+    })
 
-        // socket.broadcast.emit -> send message to everyone except creator
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
+    socket.on('createLocationMessage', coords => {
+        console.log(coords)
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longiture))
     })
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect')
 
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -10,6 +10,19 @@ describe('generateMessage', () => {
 
         expect(message.from).toBe(from)
         expect(message.text).toBe(text)
+        expect(typeof message.createdAt).toBe('number')
+    })
+})
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        const from = 'Oody'
+        const latitude = 1
+        const longitude = 100
+        const message = generateLocationMessage(from, latitude, longitude)
+
+        expect(message.from).toBe(from)
+        expect(message.url).toBe('https://www.google.com/maps?q=1,100')
         expect(typeof message.createdAt).toBe('number')
     })
 })
